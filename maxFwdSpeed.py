f677c9eea1c33669c28f869ff92cc7d1350b2da8
ikerggaldiano <iker.g.galdiano@milrem.com>
2025-08-27 13:34:37 +0300

    Update maxFwdSpeed.py

--- maxFwdSpeed.py
+++ maxFwdSpeed.py
@@ -1,5 +1,5 @@
 # maxFwdSpeed.py
-power_output_kw = 300
+power_output_kw
 efficiency=0.85
 drag_coefficient=0.8
 frontal_area_m2=2.5
@@ -10,3 +10,4 @@ power_watts = power_output_kw * 1000 * efficiency
 # Power required to overcome drag: P = F_drag * v = 0.5 * Cd * A * rho * v^3
 # Solve for v: v = (2 * P / (Cd * A * rho)) ** (1/3)
 v_max = (2 * power_watts / (drag_coefficient * frontal_area_m2 * air_density_kgm3)) ** (1/3)
+
