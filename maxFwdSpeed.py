671cbed1f0aba1f7160628bbb723d67f9467ef4f
ikerggaldiano <iker.g.galdiano@milrem.com>
2025-08-27 13:06:41 +0300

    Update maxFwdSpeed.py

--- maxFwdSpeed.py
+++ maxFwdSpeed.py
@@ -1,6 +1,9 @@
 # maxFwdSpeed.py
-v_max = 1
-def calculate_max_forward_speed(power_output_kw, efficiency=0.85, drag_coefficient=0.8, frontal_area_m2=2.5, air_density_kgm3=1.225):
+power_output_kw 
+efficiency=0.85
+drag_coefficient=0.8
+frontal_area_m2=2.5
+air_density_kgm3=1.225
     """
     Calculate the maximum forward speed of a vehicle based on powerpack power output.
 
@@ -20,6 +23,6 @@ def calculate_max_forward_speed(power_output_kw, efficiency=0.85, drag_coefficie
     # Drag force equation: F_drag = 0.5 * Cd * A * rho * v^2
     # Power required to overcome drag: P = F_drag * v = 0.5 * Cd * A * rho * v^3
     # Solve for v: v = (2 * P / (Cd * A * rho)) ** (1/3)
-    v_max = (2 * power_watts / (drag_coefficient * frontal_area_m2 * air_density_kgm3)) ** (1/3)
-    return v_max
+v_max = (2 * power_watts / (drag_coefficient * frontal_area_m2 * air_density_kgm3)) ** (1/3)
+
 
